253841a0a73f617180d1ff9ce792c9b3ffa60da5
ISHIMWE ESCHYLE BERSY <ishimweeschylebersy@gmail.com>
2021-07-26 19:05:56 +0200

    updated the serve.js

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ const express = require('express')
 
 const app = express()
 
-app.use((req, res) => {
-    res.json({message: 'requete recu :)'})
+app.use((req, res, next) => {
+    console.log('working on request...');
+    next()
+})
+
+app.use((req, res, next) => {
+    res.status(200)
+    next()
+})
+
+app.use((req, res, next) => {
+    res.json({
+        author: 'b3rking',
+        'country': 'Burundi',
+        'message': 'la requetee est passé'
+    })
+    next()
+})
+
+app.use((req, res, next) => {
+    console.log('reponse envoyé!');
+    next()
 })
 
 module.exports = app
